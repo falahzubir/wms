687152339410b20dd079a88e37002ea183acd5d2
Muhamad Iqbal <dev@iqbal.my>
2023-02-07 16:27:27 +0800

    nullable fields

---
+++ database/migrations/2023_02_07_162336_alter_orders_table_nullable_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('team_id')->nullable()->change();
+            $table->unsignedInteger('operational_model_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedInteger('operational_model_id')->change();
+            $table->unsignedBigInteger('team_id')->change();
+        });
+    }
+};
