a5bc5f6cd7b74b82b351dbf436c7250466e47798
Muhamad Iqbal <dev@iqbal.my>
2023-02-25 23:08:43 +0800

    fix

--- config/app.php
+++ config/app.php
@@ -187,7 +187,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
