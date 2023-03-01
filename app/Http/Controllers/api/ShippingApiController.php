c525b5dbe0891f4e1a3453cb23403a1ddbf13173
Muhamad Iqbal <dev@iqbal.my>
2023-03-01 14:36:02 +0800

    test

--- app/Http/Controllers/api/ShippingApiController.php
+++ app/Http/Controllers/api/ShippingApiController.php
@@ -73,7 +73,7 @@ class ShippingApiController extends ShippingController
         foreach ($companies as $company) {
 
 
-            $res = Http::post($company->url . '/api/update_tracking', $data[$company->id]);
+            $res = Http::post($company->url . '/api/update_tracking', ["data"=>1]);
 
             if (json_decode($res)->status == "success") {
 
