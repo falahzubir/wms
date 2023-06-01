36f2fca2ab24c790b1d9906a0dcc5eef5b2ed1c2
Muhamad Iqbal <dev@iqbal.my>
2023-06-02 07:39:07 +0800

    open cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('dhl-token:cron')->everySixHours();
-        // $schedule->command('send-shipping:cron')->everyFiveMinutes();
+        $schedule->command('send-shipping:cron')->everyFiveMinutes();
     }
 
     /**
