5685da7d92737e967a7aafbe4fbe432ea055ddfa
Muhamad Iqbal <dev@iqbal.my>
2023-06-10 10:04:20 +0800

    fix json address data

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,9 +10,18 @@ class Customer extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $appends = [
+        "address"
+    ];
 
     public function orders()
     {
         return $this->hasOne(Order::class);
+    }
+
+    public function getAddressAttribute()
+    {
+        // remove break line
+        return str_replace(array("\r", "\n"), '', self::select("address as old_address")->where("id",$this->id)->first()->old_address);
     }
 }
