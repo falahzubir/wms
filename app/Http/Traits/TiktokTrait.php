9c65a3824a5c9bf63438e82f813a84c9b83db252
EMZI PROPERTIES <>
2024-01-05 17:41:13 +0800

    add SL+PL in TikTok

--- app/Http/Traits/TiktokTrait.php
+++ app/Http/Traits/TiktokTrait.php
@@ -318,7 +318,7 @@ Trait TiktokTrait
             'timestamp' => $timestamp,
             'shop_id' => $shop_id,
             'order_id' => $params['ordersn'],
-            'document_type' => 'SHIPPING_LABEL'
+            'document_type' => 'SL+PL'
             // Include all the necessary parameters for signing
         ];
 
