91b765e46c20f2e3536fd84eff69cc6d726ec8a1
Falah <falah.ahmadzubir98@gmail.com>
2024-01-06 23:45:54 +0800

    Update

--- resources/views/exports/poslaju.blade.php
+++ resources/views/exports/poslaju.blade.php
@@ -1,51 +1,33 @@
 <table>
     <thead>
         <tr>
-            <td>Sender Name</td>
-            <td>Sender Email</td>
-            <td>Sender Contact No</td>
-            <td>Sender Address</td>
-            <td>Sender Postcode</td>
-            <td>Receiver Name</td>
-            <td>Receiver Email</td>
-            <td>Receiver Contact No</td>
-            <td>Receiver Address</td>
-            <td>Receiver Postcode</td>
-            <td>Item Weight (kg)</td>
-            <td>Item Width (cm)</td>
-            <td>Item Length (cm)</td>
-            <td>Item Height (cm)</td>
-            <td>Category</td>
-            <td>Sender Ref No</td>
-            <td>Item Description</td>
-            <td>Parcel Notes</td>
-            <td>COD Amount</td>
-            <td>Insurance (MYR)</td>
+            @foreach ($headers as $header)
+                <th>{{ $header }}</th>
+            @endforeach
         </tr>
     </thead>
     <tbody>
-        @foreach ($orders as $order)
+        @foreach ($orders as $key => $order)
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>{{ $order->customer->name }}</td>
-                <td></td>
-                <td>="{{ $order->customer->phone }}"</td>
-                <td>{{ $order->customer->address }}</td>
-                <td>="{{ $order->customer->postcode }}"</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>{{ $order->sales_id }}</td>
-                <td>{{ get_shipping_remarks($order)}}</td>
-                <td>{{ $order->sales_remarks }}</td>
-                <td>{{ $order->purchase_type == PURCHASE_TYPE_COD ? $order->total_price/100 : 0 }}</td>
-                <td></td>
+                @foreach ($columnName as $column)
+                    @if ($column->column_name == "blank")
+                        <td>{{ $key + 1 }}</td>
+                    @elseif ($column->column_name == "customers_name")
+                        <td>{{ $order->customer->name }}</td>
+                    @elseif ($column->column_name == "customers_phone")
+                        <td>="{{ $order->customer->phone }}"</td>
+                    @elseif ($column->column_name == "customers_address")
+                        <td>{{ $order->customer->address }}</td>
+                    @elseif ($column->column_name == "customers_postcode")
+                        <td>="{{ $order->customer->postcode }}"</td>
+                    @elseif ($column->column_name == "shipping_remarks")
+                        <td>{{ get_shipping_remarks($order)}}</td>
+                    @elseif ($column->column_name == "total_price")
+                        <td>{{ $order->purchase_type == PURCHASE_TYPE_COD ? $order->total_price/100 : 0 }}</td>
+                    @else
+                        <td>{{ $order->{$column->column_name} ?? '' }}</td>
+                    @endif
+                @endforeach
             </tr>
         @endforeach
     </tbody>
