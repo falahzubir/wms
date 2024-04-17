43f92bea26b77c1483f7df8dc5ac218ed4bdcef2
@ilias_daniel <iliasdaniel1403@gmail.com>
2024-04-17 15:12:11 +0800

    Add login_through_api and logout_through_api methods to LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -62,6 +64,55 @@ class LoginController extends Controller
         $request->validate([
             $this->username() => 'required|string',
             'password' => 'required|string',
+        ]);
+    }
+
+    public function login_through_api(Request $request)
+    {
+        $request->validate([
+            "username" => ["required"],
+            "password" => ["required"],
+        ]);
+
+        try {
+            $auth = Auth::attempt([
+                "staff_id" => $request->input("username"),
+                "password" => $request->input("password")
+            ]);
+
+            if (!$auth) {
+                return response([
+                    "status" => "error",
+                    "message" => "Unauthorized user",
+                    "data" => []
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $user = Auth::user();
+            $tokenResult = $user->createToken('jwt');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'accessToken' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Throwable $th) {
+            return response([
+                "status" => "error",
+                "message" => $th->getMessage(),
+                "data" => []
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function logout_through_api(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out'
         ]);
     }
 }
---
+++ app/Http/Controllers/FixcodeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class FixcodeController extends Controller
+{
+    public function processing_date($company_id)
+    {
+        $companies = [
+            'EH' => array('url' => 'https://bosemzi.com', 'company_id' => 1),
+            'ED' => array('url' => 'https://aa.bosemzi.com', 'company_id' => 2),
+            'QITI' => array('url' => 'https://qastg.groobok', 'company_id' => 3),
+            'INT' => array('url' => 'https://int.bosemzi.com', 'company_id' => 4),
+        ];
+
+        //check if company_id exists
+        if (!array_key_exists($company_id, $companies)) {
+            return response()->json(['message' => 'Company not found'], 404);
+        }
+
+        //get orders from company
+        $orders = Order::where('company_id', $companies[$company_id]['company_id'])->get();
+
+        //dummy url
+        $companies[$company_id]['url'] = 'http://localhost/bos';
+
+        //send orders to company
+        $response = Http::post($companies[$company_id]['url'] . '/fix_code/fix_processing_date', [
+            'sales_ids' => $orders->pluck('sales_id')->toArray(),
+        ]);
+
+        if ($response->status() == 200) {
+
+            $response = $response->json();
+
+            //update orders with processing date
+            foreach ($response as $order) {
+                Order::where('sales_id', $order['sales_id'])->update([
+                    'processed_at' => $order['dt_processing'],
+                ]);
+            }
+
+            return response()->json(['message' => 'Success'], 200);
+
+        } else {
+            return response()->json(['message' => 'Failed'], 500);
+        }
+    }
+}
