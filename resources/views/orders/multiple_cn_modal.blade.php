6ad4791e7955ea37336446c069e202680f9163e9
@ilias_daniel <iliasdaniel1403@gmail.com>
2024-05-15 16:56:17 +0800

    chore: Refactor ShippingController.php for improved readability and maintainability

--- resources/views/orders/multiple_cn_modal.blade.php
+++ resources/views/orders/multiple_cn_modal.blade.php
@@ -66,98 +66,98 @@
         }
     </style>
 </div>
-@push('orders.multiple_cn_modal')
-    <script>
-        let _order;
+@push("orders.multiple_cn_modal")
+<script>
+    let _order;
 
-        function multiple_cn(data) {
-            _order = JSON.parse(data.order);
-            if (_order.bucket_batch_id == null) {
-                Swal.fire({
-                    title: 'Error!',
-                    text: 'Please assign bucket batch first',
-                    icon: 'error',
-                    confirmButtonText: 'OK'
-                })
-                return;
-            }
-            const myModal = new bootstrap.Modal('#multiple-cn-modal', {
-                backdrop: 'static',
-                keyboard: false
-            });
-            document.getElementById('multiple-cn-modal').addEventListener('show.bs.modal', render(data))
-            document.getElementById('multiple-cn-modal').addEventListener('hidden.bs.modal', function() {
-                const body = document.getElementById("multiple-cn-modal-body");
-                const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
-                body.innerHTML = "";
-                tableBody.innerHTML = "";
+    function multiple_cn(data) {
+        _order = JSON.parse(data.order);
+        if(_order.bucket_batch_id == null){
+            Swal.fire({
+                title: 'Error!',
+                text: 'Please assign bucket batch first',
+                icon: 'error',
+                confirmButtonText: 'OK'
             })
-            myModal.show()
+            return;
         }
-
-        function render(data) {
-            const body = document.getElementById("multiple-cn-modal-body");
-            const label = document.getElementById("multiple-cn-modalLabel");
-            const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
-            const footNote = document.getElementById("multiple-cn-modal-foot-note");
-            if(data.courier_id == {{ POSMALAYSIA_ID }}){
-                footNote.innerHTML = "<small class='text-danger'>Note: For Pos Malaysia, maximum 20 CN per order allowed.</small>";
-            }
-            label.innerHTML = data.ref_no;
-            body.insertAdjacentHTML('beforeend', cn());
-            tableBody.innerHTML = tableFooter();
-            document.querySelectorAll(".multiple-cn-input").forEach(function(el) {
-                el.addEventListener("keyup", function(e) {
-                    recalculateBalance();
-                })
-            })
-            resquenceCN();
-        }
-
-        function addCn() {
+        const myModal = new bootstrap.Modal('#multiple-cn-modal', {
+            backdrop: 'static',
+            keyboard: false
+        });
+        document.getElementById('multiple-cn-modal').addEventListener('show.bs.modal', render(data))
+        document.getElementById('multiple-cn-modal').addEventListener('hidden.bs.modal', function() {
             const body = document.getElementById("multiple-cn-modal-body");
             const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
-            body.insertAdjacentHTML('beforeend', cn(true));
-            tableBody.innerHTML = tableFooter();
-            document.querySelectorAll(".multiple-cn-input").forEach(function(el) {
-                el.addEventListener("keyup", function(e) {
-                    recalculateBalance();
-                })
-            })
-            document.querySelectorAll(".delete-cn").forEach(function(el) {
-                el.addEventListener("click", function(e) {
-                    const id = e.currentTarget.dataset.id;
-                    if (document.getElementById(row-${id}) == null) {
-                        return;
-                    }
-                    document.getElementById(row-${id}).remove();
-                    resquenceCN();
-                    recalculateBalance();
-                })
-            })
-            resquenceCN();
-        }
+            body.innerHTML = "";
+            tableBody.innerHTML = "";
+        })
+        myModal.show()
+    }
 
-        function cn(allowDelete = false) {
-            const id = Math.random().toString(36).replace(/[^a-z]+/g, '');
-            let html = `<div class="card mb-3 multiple-cn-card" id="row-${id}" data-order-id="${_order.id}" data-courier-id="${_order.courier_id}">
+    function render(data) {
+        const body = document.getElementById("multiple-cn-modal-body");
+        const label = document.getElementById("multiple-cn-modalLabel");
+        const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
+        const footNote = document.getElementById("multiple-cn-modal-foot-note");
+        if(data.courier_id == {{ POSMALAYSIA_ID }}){
+            footNote.innerHTML = "<small class='text-danger'>Note: For Pos Malaysia, maximum 20 CN per order allowed.</small>";
+        }
+        label.innerHTML = data.ref_no;
+        body.insertAdjacentHTML('beforeend', cn());
+        tableBody.innerHTML = tableFooter();
+        document.querySelectorAll(".multiple-cn-input").forEach(function(el) {
+            el.addEventListener("keyup", function(e) {
+                recalculateBalance();
+            })
+        })
+        resquenceCN();
+    }
+
+    function addCn() {
+        const body = document.getElementById("multiple-cn-modal-body");
+        const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
+        body.insertAdjacentHTML('beforeend', cn(true));
+        tableBody.innerHTML = tableFooter();
+        document.querySelectorAll(".multiple-cn-input").forEach(function(el) {
+            el.addEventListener("keyup", function(e) {
+                recalculateBalance();
+            })
+        })
+        document.querySelectorAll(".delete-cn").forEach(function(el) {
+            el.addEventListener("click", function(e) {
+                const id = e.currentTarget.dataset.id;
+                if (document.getElementById(`row-${id}`) == null) {
+                    return;
+                }
+                document.getElementById(`row-${id}`).remove();
+                resquenceCN();
+                recalculateBalance();
+            })
+        })
+        resquenceCN();
+    }
+
+    function cn(allowDelete = false) {
+        const id = Math.random().toString(36).replace(/[^a-z]+/g, '');
+        let html = `<div class="card mb-3 multiple-cn-card" id="row-${id}" data-order-id="${_order.id}" data-courier-id="${_order.courier_id}">
             <div class="card-header">
                 <div class="row">
                     <div class="col-6">
                         Consignment Note <span class="cn_sequence">0</span>
                     </div>`
 
-            if (allowDelete) {
-                html += `<div class="col-6">
+        if (allowDelete) {
+            html += `<div class="col-6">
                         <i class="text-danger bx bx-trash float-end delete-cn" style="font-size: 25px;cursor:pointer" data-id="${id}"></i>
                     </div>`
-            }
+        }
 
-            html += `</div>
+        html += `</div>
             </div>`
 
-            for (const item of _order.items) {
-                html += `<div class="card-body p-2">
+        for (const item of _order.items) {
+            html += `<div class="card-body p-2">
                 <div class="row m-2">
                     <div class="col-8 my-auto">
                         <div class="row ">
@@ -171,82 +171,81 @@
                     </div>
                 </div>
             </div>`
-            }
+        }
 
-            html += `<div class="card-footer" style="font-size:12px;">
+        html += `<div class="card-footer" style="font-size:12px;">
                 Leave the product with 0 quantity if you want to exclude it from CN
             </div>
         </div>`
 
-            return html;
-        }
+        return html;
+    }
 
-        function tableFooter() {
-            let html = "";
-            for (let i = 0; i < _order.items.length; i++) {
-                const item = _order.items[i];
-                html += `<tr id="row-${item.id}">
+    function tableFooter() {
+        let html = "";
+        for (let i = 0; i < _order.items.length; i++) {
+            const item = _order.items[i];
+            html += `<tr id="row-${item.id}">
                 <td>${item.product.name}</td>
                 <td class="text-center" id="quantity">${item.quantity}</td>
                 <td class="text-center" id="balance">${item.cn_qty ?? 0}</td>`
-                if (i == 0) {
-                    html +=
-                        `<td class="text-center align-middle" rowspan="${_order.items.length}">${document.querySelectorAll('.multiple-cn-card').length}</td>`
-                }
-                html += `</tr>`
+            if (i == 0) {
+                html += `<td class="text-center align-middle" rowspan="${_order.items.length}">${document.querySelectorAll('.multiple-cn-card').length}</td>`
             }
-
-            return html;
+            html += `</tr>`
         }
 
-        function resquenceCN() {
-            const list = document.querySelectorAll(".cn_sequence");
+        return html;
+    }
 
-            for (let i = 0; i < list.length; i++) {
-                const element = list[i];
-                element.innerHTML = i + 1;
+    function resquenceCN() {
+        const list = document.querySelectorAll(".cn_sequence");
+
+        for (let i = 0; i < list.length; i++) {
+            const element = list[i];
+            element.innerHTML = i + 1;
+        }
+    }
+
+    function recalculateBalance() {
+        const sum = [];
+
+        for (const element of document.querySelectorAll(".multiple-cn-input")) {
+            const id = element.dataset.id;
+            const index = sum.findIndex(s => s.id == id);
+            let val = parseInt(element.value);
+
+            if (Number.isNaN(val)) {
+                continue;
+            }
+
+            if (index == -1) {
+                sum.push({
+                    id: id,
+                    t: val
+                });
+            } else {
+                sum[index].t = sum[index].t + val;
             }
         }
 
-        function recalculateBalance() {
-            const sum = [];
+        for (const s of sum) {
+            const i = _order.items.findIndex(i => i.id == s.id);
+            let val = _order.items[i].quantity - s.t;
 
-            for (const element of document.querySelectorAll(".multiple-cn-input")) {
-                const id = element.dataset.id;
-                const index = sum.findIndex(s => s.id == id);
-                let val = parseInt(element.value);
-
-                if (Number.isNaN(val)) {
-                    continue;
-                }
-
-                if (index == -1) {
-                    sum.push({
-                        id: id,
-                        t: val
-                    });
-                } else {
-                    sum[index].t = sum[index].t + val;
-                }
+            if (val < 1) {
+                val = 0;
             }
 
-            for (const s of sum) {
-                const i = _order.items.findIndex(i => i.id == s.id);
-                let val = _order.items[i].quantity - s.t;
+            _order.items[i].cn_qty = val;
 
-                if (val < 1) {
-                    val = 0;
-                }
-
-                _order.items[i].cn_qty = val;
-
-            }
-
-            const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
-            tableBody.innerHTML = tableFooter();
         }
 
-        function generateMultipleCN() {
+        const tableBody = document.getElementById("multiple-cn-modal-table-footer-body");
+        tableBody.innerHTML = tableFooter();
+    }
+
+    function generateMultipleCN() {
             // validate
             const errors = [];
             var arr_data = []; //for all CN
@@ -521,5 +520,5 @@
             }
 
         }
-    </script>
+</script>
 @endpush
