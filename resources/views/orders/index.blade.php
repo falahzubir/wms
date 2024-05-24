4d43115256262bf8b858898079ff696ca89bdc41
Shahrul Alif <shahrulalif.ms@gmail.com>
2024-05-24 10:45:24 +0800

    update

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2031,8 +2031,8 @@
             }
             let ws;
             document.addEventListener("DOMContentLoaded",function(){
-                ws = io('https://websocketnew.groobok.com'); //live (need domain instead of ip address)
-                // ws = io('http://152.42.168.240:8080'); //development (need ip addrrss instead)
+                // ws = io('https://websocketnew.groobok.com'); //live (need domain instead of ip address)
+                ws = io('https://152.42.168.240:443'); //development (need ip addrrss instead)
                 // ws = new WebSocket('ws://206.189.144.234:8080');
 
                 // Event listener for when the connection is established
