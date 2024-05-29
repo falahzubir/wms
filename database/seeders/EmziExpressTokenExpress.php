fa2cdf226b795228fde5e12044360ce251823e7d
@ilias_daniel <iliasdaniel1403@gmail.com>
2024-05-29 09:27:44 +0800

    refactor: Update EmziExpressTokenExpress seeder to create access tokens for multiple companies

--- database/seeders/EmziExpressTokenExpress.php
+++ database/seeders/EmziExpressTokenExpress.php
@@ -14,13 +14,64 @@ class EmziExpressTokenExpress extends Seeder
      */
     public function run(): void
     {
-        $emziExpress = AccessToken::where('type', 'emzi-express')->first();
+        $emziExpressQiti = AccessToken::where('type', 'emzi-express')->where('company_id', 3)->first();
 
-        if (!$emziExpress) {
-            $emziExpress = AccessToken::create([
+        if (!$emziExpressQiti) {
+            $emziExpressQiti = AccessToken::create([
                 'type' => 'emzi-express',
                 'client_id' => 'd566bd05-0924-438d-b64b-33e5f576bcf3',
                 'company_id' => 3, #qiti
+                'name' => 'Emzi Express Authentification',
+                'token' => 'eyJ0eXA',
+                'additional_data' => ([
+                    'email' => 'emziqiti@gmail.com',
+                ]),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
+        $emziExpressEH = AccessToken::where('type', 'emzi-express')->where('company_id', 1)->first();
+
+        if (!$emziExpressEH) {
+            $emziExpressEH = AccessToken::create([
+                'type' => 'emzi-express',
+                'client_id' => 'd566bd05-0924-438d-b64b-33e5f576bcf3',
+                'company_id' => 1, #eh
+                'name' => 'Emzi Express Authentification',
+                'token' => 'eyJ0eXA',
+                'additional_data' => ([
+                    'email' => 'emziqiti@gmail.com',
+                ]),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
+        $emziExpressED = AccessToken::where('type', 'emzi-express')->where('company_id', 2)->first();
+
+        if (!$emziExpressED) {
+            $emziExpressED = AccessToken::create([
+                'type' => 'emzi-express',
+                'client_id' => 'd566bd05-0924-438d-b64b-33e5f576bcf3',
+                'company_id' => 2, #ed
+                'name' => 'Emzi Express Authentification',
+                'token' => 'eyJ0eXA',
+                'additional_data' => ([
+                    'email' => 'emziqiti@gmail.com',
+                ]),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
+        $emziExpressEI = AccessToken::where('type', 'emzi-express')->where('company_id', 4)->first();
+
+        if (!$emziExpressEI) {
+            $emziExpressEI = AccessToken::create([
+                'type' => 'emzi-express',
+                'client_id' => 'd566bd05-0924-438d-b64b-33e5f576bcf3',
+                'company_id' => 4, #EI
                 'name' => 'Emzi Express Authentification',
                 'token' => 'eyJ0eXA',
                 'additional_data' => ([
