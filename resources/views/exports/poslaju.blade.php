3ac65c3c87c95403780ff8bbd36048eee2eb871d
@ilias_daniel <iliasdaniel1403@gmail.com>
2024-07-09 09:31:50 +0800

    refactor: Update shipping cost calculation in ShippingController

--- resources/views/exports/poslaju.blade.php
+++ resources/views/exports/poslaju.blade.php
@@ -11,6 +11,131 @@
             $globalCounter = 1;
         @endphp
         @foreach ($orders as $key => $order)
+            @if ($order->shippings->isEmpty())
+                <tr>
+                    @foreach ($columnName as $column)
+                        @if ($column->column_name == "blank")
+                            <td>{{ $globalCounter++ }}</td>
+                        @elseif ($column->column_name == "order_id")
+                            <td>{{ $order->id }}</td>
+                        @elseif ($column->column_name == "companies_name")
+                            <td>{{ optional($order->company)->name }}</td>
+                        @elseif ($column->column_name == "companies_phone")
+                            <td>{{ $order->company->phone }}</td>
+                        @elseif ($column->column_name == "companies_address")
+                            <td>{{ $order->company->address }}</td>
+                        @elseif ($column->column_name == "companies_postcode")
+                            <td>{{ $order->company->postcode }}</td>
+                        @elseif ($column->column_name == "companies_city")
+                            <td>{{ $order->company->city }}</td>
+                        @elseif ($column->column_name == "companies_state")
+                            <td>{{ $order->company->state }}</td>
+                        @elseif ($column->column_name == "companies_country")
+                            <td>{{ $order->company->country }}</td>
+                        @elseif ($column->column_name == "customers_name")
+                            <td>{{ $order->customer->name }}</td>
+                        @elseif ($column->column_name == "customers_phone")
+                            <td>{{ $order->customer->phone }}</td>
+                        @elseif ($column->column_name == "customers_phone_2")
+                            <td>{{ $order->customer->phone_2 }}</td>
+                        @elseif ($column->column_name == "customers_address")
+                            <td>{{ $order->customer->address }}</td>
+                        @elseif ($column->column_name == "customers_postcode")
+                            <td>{{ $order->customer->postcode }}</td>
+                        @elseif ($column->column_name == "customers_city")
+                            <td>{{ $order->customer->city }}</td>
+                        @elseif ($column->column_name == "customers_state")
+                            <td>{{ get_state($order->customer->state) }}</td>
+                        @elseif ($column->column_name == "customers_country")
+                            <td>
+                                @switch($order->customer->country)
+                                    @case('1')
+                                        MY
+                                        @break
+                                    @case('2')
+                                        ID
+                                        @break
+                                    @case('3')
+                                        SG
+                                        @break
+                                @endswitch
+                            </td>
+                        @elseif ($column->column_name == "purchase_type")
+                            <td>
+                                @switch($order->purchase_type)
+                                    @case('1')
+                                        COD
+                                        @break
+                                    @case('2')
+                                        Paid
+                                        @break
+                                    @case('3')
+                                        Installment
+                                        @break
+                                @endswitch
+                            </td>
+                        @elseif ($column->column_name == "operational_models_name")
+                            <td>{{ get_operational_details($order->operational_model_id) }}</td>
+                        @elseif ($column->column_name == "payment_type_name")
+                            <td>{{ get_payment_name($order->payment_type) }}</td>
+                        @elseif ($column->column_name == "couriers_name")
+                            <td>{{ $order->courier->name }}</td>
+                        @elseif ($column->column_name == "total_price")
+                            <td>{{ $order->total_price / 100 }}</td>
+                        @elseif ($column->column_name == "quantity")
+                            <td>{{ get_order_items($order->id)['sumQuantity'] }}</td>
+                        @elseif ($column->column_name == "weight")
+                            <td>{{ get_order_items($order->id)['sumWeight'] }}g</td>
+                        @elseif ($column->column_name == "item_description")
+                            <td>{{ get_shipping_remarks($order) }}</td>
+                        @elseif ($column->column_name == "date_insert")
+                            <td>{{ $order->created_at }}</td>
+                        @elseif ($column->column_name == "shipping_date")
+                            <td>-</td>
+                        @elseif ($column->column_name == "tracking_number")
+                            <td>-</td>
+                        @elseif ($column->column_name == "scan_date")
+                            <td>-</td>
+                        @elseif ($column->column_name == "pic_scan")
+                            <td>-</td>
+                        @elseif ($column->column_name == "delivered_date")
+                            <td>-</td>
+                        @elseif ($column->column_name == "order_pic")
+                            <td>
+                                @php
+                                    $salesId = $order->sales_id;
+                                    $staffNames = json_decode($staffMain, true);
+                                @endphp
+                                @if (!empty($staffNames))
+                                    @foreach ($staffNames as $staff)
+                                        @if ($staff['sales_id'] == $salesId)
+                                            {{ $staff['staff_name'] }}
+                                            @php $found = true; @endphp
+                                        @endif
+                                    @endforeach
+                                    @if (!isset($found))
+                                        -
+                                    @endif
+                                @else
+                                    -
+                                @endif
+                            </td>
+                        @elseif ($column->column_name == "state_group")
+                            <td>-</td>
+                        @elseif ($column->column_name == "total_weight")
+                            <td>-</td>
+                        @elseif ($column->column_name == "weight_category")
+                            <td>-</td>
+                        @elseif ($column->column_name == "shipping_cost")
+                            <td>-</td>
+                        @elseif ($column->column_name == "shipping_cost_product_quantity")
+                            <td>-</td>
+                        @else
+                            <td>{{ $order->{$column->column_name} ?? '' }}</td>
+                        @endif
+                    @endforeach
+                </tr>
+        @else
             @foreach ($order->shippings as $shippingIndex => $shipping)
                 <tr>
                     @foreach ($columnName as $column)
@@ -169,6 +294,7 @@
                     @endforeach
                 </tr>
             @endforeach
+        @endif
         @endforeach
     </tbody>
 </table>
