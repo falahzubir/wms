37fc92d9598bec6bfd232ae9101dabea3c40a15f
Falah <falah.ahmadzubir98@gmail.com>
2024-09-18 16:50:55 +0800

    Update alert

--- app/Http/Controllers/CountryListController.php
+++ app/Http/Controllers/CountryListController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use Illuminate\Http\Request;
-use RealRashid\SweetAlert\Facades\Alert;
 
 class CountryListController extends Controller
 {
